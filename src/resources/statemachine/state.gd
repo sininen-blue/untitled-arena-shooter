f572b655a8732d8aaa177ad11794066929353945
Neil Alaan <alaanneil@gmail.com>
2026-09-26 20:38:44 +0800

    state machine

---
+++ src/resources/statemachine/state.gd
@@ -0,0 +1,24 @@
+class_name State
+extends Node
+
+var state_machine: StateMachine = null
+
+
+func enter() -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func update(_delta: float) -> void:
+	pass
+
+
+func physics_update(_delta: float) -> void:
+	pass
+
+
+func handle_input(_event: InputEvent) -> void:
+	pass
---
+++ src/resources/statemachine/state_machine.gd
@@ -0,0 +1,62 @@
+class_name StateMachine
+extends Node
+
+signal changed_state(state: State)
+
+@export var initial_state: State
+
+var current_state: State
+var previous_state: State
+
+var states: Dictionary = { }
+var state_components: Dictionary = { }
+
+
+func _ready() -> void:
+	var children: Array[Node] = get_children()
+	for child: Node in children:
+		if child is State:
+			states[child] = child
+			child.state_machine = self
+
+	if initial_state:
+		change_state(initial_state)
+
+
+func _process(delta: float) -> void:
+	if current_state:
+		current_state.update(delta)
+
+
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.physics_update(delta)
+
+
+func _input(event: InputEvent) -> void:
+	if current_state:
+		current_state.handle_input(event)
+
+
+func change_state(new_state: State) -> void:
+	if current_state:
+		previous_state = current_state
+		current_state.exit()
+
+	current_state = states.get(new_state)
+
+	if current_state:
+		changed_state.emit(current_state)
+		current_state.enter()
+
+
+func get_current_state_name() -> String:
+	if current_state:
+		return current_state.name
+	return "unavailable"
+
+
+func get_previous_state_name() -> String:
+	if previous_state:
+		return previous_state.name
+	return "unavailable"
